f64515114c5352ae371b4474647c6241ed3771ee
OscarSilvaOfficial <oscarkaka222@gmail.com>
2021-06-08 23:41:17 -0300

    update

--- custom_user/models.py
+++ custom_user/models.py
@@ -4,26 +4,24 @@ from django.core import validators
 from django.utils import timezone
 from django.utils.translation import ugettext_lazy as _
 from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
-
+from django.contrib.auth.models import UserManager
+from django.apps import apps
+from django.contrib.auth.hashers import make_password
 class UserManager(BaseUserManager):
     
-  def _create_user(self, username, email, password, is_staff, is_superuser, **extra_fields):
-    now = timezone.now()
-    
-    if not username:
-        raise ValueError(_('The given username must be set'))
-    
-    email = self.normalize_email(email)
-    user = self.model(
-      username=username, email=email,
-      is_staff=is_staff, is_active=True,
-      is_superuser=is_superuser, last_login=now,
-      date_joined=now, **extra_fields
-    )
-    user.set_password(password)
-    user.save(using=self._db)
-    
-    return user
+  def _create_user(self, username, email, password, **extra_fields):
+
+        if not username:
+            raise ValueError('The given username must be set')
+          
+        email = self.normalize_email(email)
+        GlobalUserModel = apps.get_model(self.model._meta.app_label, self.model._meta.object_name)
+        username = GlobalUserModel.normalize_username(username)
+        user = self.model(username=username, email=email, **extra_fields)
+        user.password = make_password(password)
+        user.save(using=self._db)
+        
+        return user
     
   def create_user(self, username, email=None, password=None, **extra_fields):
     return self._create_user(username, email, password, False, False,**extra_fields)
